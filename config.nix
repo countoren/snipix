473df3cf3f3723d08cb42c145fe828e74c010173
Oren Rozen <oren.rozen@topmanage.com>
2018-05-25 16:36:24 -0500

    add macvim

--- config.nix
+++ config.nix
@@ -3,6 +3,28 @@
 	allowUnfree = true;
 
 	packageOverrides = pkgs_: with pkgs_; {
-		mvim = import ./vim { inherit pkgs; };
-	};
+		vim = import ./vim { inherit pkgs; };
+    macvim = stdenv.mkDerivation {
+      name = "macvim-147";
+      src = fetchurl {
+        url = "https://github.com/macvim-dev/macvim/releases/download/snapshot-147/MacVim.dmg";
+        sha256 = "07szhx043ixym8n15n5xn9g5mjf1r8zi28hgdbpyf07vrfymc0zg";
+      };
+      buildInputs = [ p7zip ];
+      buildCommand = ''
+        7z x $src
+        cd MacVim
+        mkdir -p $out/Applications
+        cp -rfv MacVim.app $out/Applications
+        chmod 755 $out/Applications/MacVim.app/Contents/MacOS/* \
+                  $out/Applications/MacVim.app/Contents/bin/*
+        mkdir -p $out/bin
+        ln -sf $out/Applications/MacVim.app/Contents/bin/mvim $out/bin/mvim
+        ln -sf $out/bin/mvim $out/bin/vim
+        ln -sf $out/bin/mvim $out/bin/vi
+        ln -sf $out/bin/mvim $out/bin/gvim
+      '';
+    };
+
+  };
 }
