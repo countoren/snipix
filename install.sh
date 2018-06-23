47ce0a1925606381fe1840e816fccb2349611c97
countoren <countoren@gmail.com>
2018-06-22 23:51:25 -0500

    using absolute path for nix-env

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ echo "Installing nix"
 curl https://nixos.org/nix/install | sh
 source /etc/bashrc
 source /etc/profile
-if [ ! -d "~/.nixpkgs" ]; then
+if [ ! -d ~/.nixpkgs ]; then
 	echo "Installing git"
-	nix-env -i git
+	/nix/var/nix/profiles/default/bin/nix-env -i git
 	echo "cloning my nix packages"
 	git clone https://github.com/countoren/nixpkgs.git ~/.nixpkgs
 fi
 echo "installing HomeInstall"
-nix-env -i homeInstall
+/nix/var/nix/profiles/default/bin/nix-env -i homeInstall
 echo "running homeInstall"
 homeInstall
