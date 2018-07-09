6aeb851b88c4e8f7304dd6051738c397b7e7f614
countoren <countoren@gmail.com>
2018-07-09 08:40:30 -0500

    add toDotFileWithDeps method

--- HomeInstall/toDotFileExp.nix
+++ HomeInstall/toDotFileExp.nix
@@ -1,14 +1,23 @@
-{stdenv}: dotfilePath:
-let dotfileName = baseNameOf dotfilePath;
-in
-stdenv.mkDerivation {
-    name = dotfileName;
-
-    phases = [ "installPhase" ];
-
-    src = dotfilePath;
-
-    installPhase = ''
-      install -dm 755 $out/userHome
-      substitute $src $out/userHome/.''+dotfileName;
+{mkDerivation, writeText}:  
+let 
+  createDotExp = 
+    dotFileSrc: dotfileName: 
+    mkDerivation {
+        name = dotfileName;
+        phases = [ "installPhase" ];
+        src = dotFileSrc;
+        installPhase = ''
+          install -dm 755 $out/userHome
+          substitute $src $out/userHome/.''+dotfileName;
+  };
+in {
+  toDotfile = dfPath: createDotExp dfPath (baseNameOf dfPath); 
+  toDotfileWithDeps = dfPath: deps: 
+  let 
+    fileName = (baseNameOf dfPath);
+    fileContent = import dfPath deps;
+    dotfileNix = writeText fileName fileContent;
+  in
+    createDotExp dotfileNix fileName;
 }
+
