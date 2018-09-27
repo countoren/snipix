2dfe7f04b5b569bff2ee9a0556ed6ac9ee91c53d
countoren <countoren@gmail.com>
2018-09-27 07:50:41 +0300

    change uninstall script to fit nix 2.1.2

--- uninstall.sh
+++ uninstall.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-
-echo "Restoring etc/bashrc"
-sudo mv /etc/bashrc.backup-before-nix /etc/bashrc
-
-echo "Restoring etc/zshrc"
-sudo mv /etc/zshrc.backup-before-nix /etc/zshrc
-
+echo "Uninstalling nix"
 echo "Removing nix"
-sudo rm -rf /etc/nix /nix /var/root/.nix-profile /var/root/.nix-defexpr /var/root/.nix-channels /Users/orenrozen/.nix-profile /Users/orenrozen/.nix-defexpr /Users/orenrozen/.nix-channels
+rm -rf $HOME/{.nix-channels,.nix-defexpr,.nix-profile,.config/nixpkgs}
+sudo rm -rf /nix
+
