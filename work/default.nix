4e9a5e3125e41d652b77cbcc938bc90e0baea418
Oren Rozen <countoren@gmail.com>
2023-04-17 11:27:05 -0400

    add work shell commands

---
+++ work/default.nix
@@ -0,0 +1,40 @@
+{ pkgs ? import <nixpkgs>{} 
+, lib  ? pkgs.lib
+, writeShellScript ? pkgs.writeShellScript
+, runCommand ? pkgs.runCommand
+, git ? "${pkgs.git}/bin/git"
+, user ? "exampleUser"
+, prefix ? "work"
+, toUpper ? pkgs.lib.toUpper
+, fzf ? ''${pkgs.fzf}/bin/fzf -i -e +s \
+        --reverse \
+        --ansi \
+        --tiebreak=index \
+        --bind "ctrl-J:down" \
+        --bind "ctrl-K:up"''
+}:
+let 
+  getGitRootFolder = ''${git} rev-parse --show-toplevel'';
+  getFlakes = ''(builtins.getFlake "'$(${getGitRootFolder})'")'';
+  ssh = ''${pkgs.openssh}/bin/ssh'';
+  commands = 
+  lib.attrValues (lib.fix (self: lib.mapAttrs writeShellScript (
+  {
+    #General
+    projectFolder = getGitRootFolder;
+    sshDeltaServer = ''${ssh} $(whoami)"@"192.168.56.220'';
+  } 
+  )));
+in
+runCommand prefix {
+  name = prefix;
+  version = "1.0.0";
+  }
+  ''
+    mkdir -p $out/bin
+    ${lib.concatMapStringsSep " " (c: ''
+      ln -nfs ${c} $out/bin/${
+        if prefix == c.name then c.name else prefix+"-"+c.name
+        }
+    '') commands}
+  ''
