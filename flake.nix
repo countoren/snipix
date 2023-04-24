2d99f21e40043c7d88563642d8e6a16542863f67
Oren Rozen <countoren@gmail.com>
2023-04-23 21:55:15 -0400

    remove system from templates

--- flake.nix
+++ flake.nix
@@ -1,15 +1,14 @@
 {
-  inputs.flake-utils.url = github:numtide/flake-utils;
-
-  outputs = { self, nixpkgs, flake-utils }:
-  #flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-  flake-utils.lib.eachDefaultSystem (system:
-  let pkgs = nixpkgs.legacyPackages.${system};
-  in rec {
-    templates.basic = {
-      description = "basic-flake"; 
-      path = ./basic; 
+  outputs = { self }:
+  # flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+  # flake-utils.lib.eachDefaultSystem (system:
+  rec {
+    templates = {
+      basic = {
+        description = "basic flake"; 
+        path = ./basic; 
+      };
+      default = templates.basic;
     };
-    templates.default = templates.basic;
-  });
+  };
 }
