ac4d1d34cd3ad677d174edec769120fd86ef2fa9
countoren <countoren@gmail.com>
2023-08-20 14:01:08 -0400

    change prefix to sx instead of tmp
    add some more save function to handle saving in more stright forward way
    cleanup un used code

--- templates/default.nix
+++ templates/default.nix
@@ -1,12 +1,17 @@
 { pkgs ? (builtins.getFlake (toString ../.)).inputs.nixpkgs.legacyPackages.${builtins.currentSystem}
 , templates ? import ./templates.nix
 # Prefix to be appended to all 
-, prefix ? "tmp"
+, prefix ? "sx"
+
+, nix ? "${pkgs.nix}/bin/nix"
+, lib ? pkgs.lib
+, writeShellScriptBin ? pkgs.writeShellScriptBin
+
+, git ? "${pkgs.git}/bin/git"
 # This string is repesenting the absolute pass of your templates folder
 # if use in standalone the path could be brought by this command
 # otherwise replace with hardcoded string path (will be used in a shell script)
 , templatesFolder ? ''$(${git} rev-parse --show-toplevel)''
-, nix ? "${pkgs.nix}/bin/nix"
 
 , highlight ? "${pkgs.highlight}/bin/highlight"
 , fzf ? "${pkgs.fzf}/bin/fzf"
@@ -14,35 +19,25 @@
 , listAndSelectTool? pkgs.writeShellScript "select" ''
   find $1 -type f -not -path './.git/*' | ${fzf} -1 --preview="${highlight} -O ansi {}"       
 ''
+# this command will be fed by a file chosed by the user which contain the snippet
+, snipTool ? pkgs.writeShellScript "snipTool" ''cat $1 | ${pkgs.xclip}/bin/xclip -selection c && cat $1''
 
 # Default nix install command for stand alone templates(not with Nixos or HomeManager)
-# The command should be fed as a string with references in order to fed into writeShellScript
+# The command should be fed as a string with references in order to feed it into writeShellScript
 # if used in nixos should be replaced with something like nixos-rebuild switch ...
 , installCommand ? ''
     ${nix} profile remove $(${nix} profile list | grep '${prefix}$' | cut -d ' ' -f 4)
     ${nix} profile install -f ${templatesFolder}/default.nix
 ''
-, lib ? pkgs.lib
-, writeShellScriptBin ? pkgs.writeShellScriptBin
-, git ? "${pkgs.git}/bin/git"
 # a difftool that will be fed with 2 filepath to compare in case of conflict
 # between a target file and a template file
 , difftool ? pkgs.writeShellScript "vimdiff" ''
   ${pkgs.vim}/bin/vimdiff $1 $2
 ''
-# this command will be fed by a file chosed by the user which contain the snippet
-, snipTool ? pkgs.writeShellScript "snipTool" ''cat $1 | ${pkgs.xclip}/bin/xclip -selection c && cat $1''
-# , difftool ? pkgs.writeShellScript "vimdiff" ''
-#       nvim-client-send "tabe $1 | vert diffs $2" 
-# ''
-
-, templateWithDiff ? { templatesAddress, difftool}: templateName: ''
-    ${nix} flake init -t ${templatesAddress}\#${templateName} 2>&1 | grep 'refusing\|merge' | awk '{ print $NF }' | xargs -n 2 ${difftool} 
-''
 }:
 let commands = lib.fix (self: lib.mapAttrs pkgs.writeShellScript 
   ({
-    inherit installCommand;
+    utils-install-command = installCommand;
     init = ''
       ${nix} flake init -t ${templatesFolder}\#$1
     '';
@@ -51,48 +46,66 @@ let commands = lib.fix (self: lib.mapAttrs pkgs.writeShellScript
      ${snipTool} $(${listAndSelectTool} ${templatesFolder}/$1 )
     '';
 
-    initWithDiff = ''
+    init-with-diff = ''
       echo 'merge conflict files running the following command:'
       echo "${difftool} $1 $2"
         
       ${self.init} $1 2>&1 | grep 'refusing\|merge' | awk '{ print $NF }' | xargs -n 2 ${difftool}
     '';
 
-    save-template = ''
-      read -p "Template Name:" name 
-      read -p "Template Desc:" desc 
+    save-basic = ''
+      echo "creating template folder $1 and copying the current folder into it..."
+      mkdir -p ${templatesFolder}/$1 
+      cp -r . ${templatesFolder}/$1 
 
-      echo "creating template folder $name and copying the current folder into it..."
-      mkdir -p ${templatesFolder}/$name 
-      cp -r . ${templatesFolder}/$name 
-
-      echo 'Edit Files?[press y to edit or any key to continue]'
-      read -n1 INPUT && [[ "$INPUT" == "y" ]] && $EDITOR ${templatesFolder}/$name && \
+      [[ "$INPUT" == "y" ]] && $EDITOR ${templatesFolder}/$1 && \
       echo 'Waiting, press any key to contine...' && read -n1 K 
 
       echo "cd into templates and adding folder to git"
       pushd ${templatesFolder}
-      ${git} add $name
+      ${git} add $1
       echo "cd back to last folder"
       popd
 
-      echo "Inserting $name into the templates list"
-      echo "// { $name = { description = \"$desc\"; path = ./$name; }; }" >> ${templatesFolder}/templates.nix
+      echo "Inserting $1 into the templates list"
+      echo "// { $1 = { description = \"$2\"; path = ./$name; }; }" >> ${templatesFolder}/templates.nix
 
-      ${self.installCommand} 
+      ${self.utils-install-command} 
     '';
+
+    save-p = ''
+      read -p "Template Name:" name 
+      read -p "Template Desc:" desc 
+      echo 'Edit Files?[press y to edit or any key otherwise]'
+      read -n1 INPUT
+      ${self.save-basic} $name $desc $INPUT
+    '';
+    utils-get-flake-desc = "${nix} flake metadata  | grep 'Description' | sed 's/Description.*//' ";
+    save = ''
+      name=$(basename `pwd`)
+      desc=$(${self.utils-get-flake-desc})
+      INPUT="n"
+      ${self.save-basic} $name $([[ ! -z $desc ]] || echo 'N/A') $input
+    '';
+    save-edit = ''
+      name=$(basename `pwd`)
+      desc=$(${self.utils-get-flake-desc})
+      INPUT="y"
+      ${self.save-basic} $name $([[ ! -z $desc ]] || echo 'N/A') $input
+    '';
+
   } // lib.attrsets.concatMapAttrs (name: { description, path }: {
-    "${prefix}-${name}" = '' ${self.initWithDiff} ${name} '';
+    "${name}" = '' ${self.init-with-diff} ${name} '';
     "snip-${name}" = ''${self.snip} ${name}'';
-    "${prefix}-${name}-edit" = ''$EDITOR ${templatesFolder}/${name} '';
-    "${prefix}-${name}-noDiff" = '' ${self.init} ${name} '';
-    "${prefix}-${name}-description" = '' echo "${description}" '';
+    "${name}-edit" = ''$EDITOR ${templatesFolder}/${name} '';
+    "${name}-noDiff" = '' ${self.init} ${name} '';
+    "${name}-description" = '' echo "${description}" '';
   }) templates)
 
 );
 # nix build -f default.nix --argstr templatesFolder /home/p1n3/nixpkgs/templates 
 in lib.fix (self: pkgs.symlinkJoin {
   name = prefix;
-  passthru.commands = lib.mapAttrs pkgs.writeShellScriptBin commands;
-  paths = (lib.attrValues self.passthru.commands);
+  passthru.commands = lib.mapAttrs (name: pkgs.writeShellScriptBin "${prefix}-${name}") commands;
+  paths = lib.attrValues self.passthru.commands;
 })
